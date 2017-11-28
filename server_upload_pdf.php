1d47107dde9a4104ece3c0dcccd32b6d6c8835a7
nandhujit <nandhujit@gmail.com>
2017-11-28 18:31:59 +0530

    Create server_upload_pdf.php

---
+++ server_upload_pdf.php
@@ -0,0 +1,68 @@
+<?php
+ 
+ServerConfig();
+
+$PdfUploadFolder = 'PdfUploadFolder/';
+ 
+$ServerURL = 'https://group5sep.000webhostapp.com/'.$PdfUploadFolder;
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+ 
+ if(isset($_POST['name']) and isset($_FILES['pdf']['name'])){
+
+ $con = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
+ 
+ $PdfName = $_POST['name'];
+ 
+ $PdfInfo = pathinfo($_FILES['pdf']['name']);
+ 
+ $PdfFileExtension = $PdfInfo['extension'];
+ 
+ $PdfFileURL = $ServerURL . GenerateFileNameUsingID() . '.' . $PdfFileExtension;
+ 
+ $PdfFileFinalPath = $PdfUploadFolder . GenerateFileNameUsingID() . '.'. $PdfFileExtension;
+ 
+ try{
+ 
+ move_uploaded_file($_FILES['pdf']['tmp_name'],$PdfFileFinalPath);
+ 
+ $InsertTableSQLQuery = "INSERT INTO material (  Material_ID, URL_image ) VALUES ('$PdfName' ,'$PdfFileURL' ) ;";
+
+ mysqli_query($con,$InsertTableSQLQuery);
+
+ }catch(Exception $e){} 
+ mysqli_close($con);
+ 
+ }
+}
+
+function ServerConfig(){
+ 
+define('HostName','localhost');
+define('HostUser','id3586610_id3586610_root');
+define('HostPass','12345678');
+define('DatabaseName','id3586610_printbook');
+ 
+}
+
+function GenerateFileNameUsingID(){
+ 
+ $con2 = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
+ 
+ $GenerateFileSQL = "SELECT max(id) as id FROM material";
+ 
+ $Holder = mysqli_fetch_array(mysqli_query($con2,$GenerateFileSQL));
+
+ mysqli_close($con2);
+ 
+ if($Holder['id']==null)
+ {
+ return 1;
+ }
+ else
+ {
+ return ++$Holder['id'];
+ }
+}
+
+?>
